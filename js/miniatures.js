7114ac646e6a39d6c7fc2fa47aad1fd8808e2182
4nk1r <4nk1r.dev@gmail.com>
2025-12-16 13:41:35 +0500

    implemented photos list filters

--- js/miniatures.js
+++ js/miniatures.js
@@ -1,7 +1,18 @@
 import {photoMiniatureClickListener} from './fullscreen-preview.js';
+import {getPhotos} from './api.js';
+import {showErrorMessage} from './modal-messages.js';
+import {debounce} from './utils.js';
+
+const FILTER_DEBOUNCE_DELAY = 500;
+const RANDOM_PHOTOS_COUNT = 10;
+
+let originalPhotos = [];
 
 const template = document.querySelector('template#picture').content;
 const container = document.querySelector('.pictures');
+
+const filtersContainer = document.querySelector('.img-filters');
+const filterButtons = document.querySelectorAll('.img-filters__button');
 
 const insertPhotoMiniature = (photo) => {
   const element = template.cloneNode(true);
@@ -24,6 +35,70 @@ const insertPhotoMiniature = (photo) => {
   container.appendChild(element);
 };
 
-export const displayPhotos = (photosList) => {
+const clearPhotos = () => {
+  container.querySelectorAll('.picture').forEach((item) => item.remove());
+};
+
+const displayPhotos = (photosList) => {
+  clearPhotos();
   photosList.forEach(insertPhotoMiniature);
+};
+
+const applyFilter = (filterId) => {
+  let filteredPhotos = [];
+
+  if (filterId === 'filter-default') {
+    filteredPhotos = [...originalPhotos];
+  }
+
+  if (filterId === 'filter-random') {
+    filteredPhotos = [...originalPhotos]
+      .sort(() => Math.random() - 0.5)
+      .slice(0, RANDOM_PHOTOS_COUNT);
+  }
+
+  if (filterId === 'filter-discussed') {
+    filteredPhotos = [...originalPhotos]
+      .sort((a, b) => b.comments.length - a.comments.length);
+  }
+
+  displayPhotos(filteredPhotos);
+};
+
+const debouncedApplyFilter = debounce(applyFilter, FILTER_DEBOUNCE_DELAY);
+
+const onFilterClick = (evt) => {
+  const { id } = evt.target;
+
+  filterButtons.forEach((button) => {
+    button.classList.toggle(
+      'img-filters__button--active',
+      button === evt.target
+    );
+  });
+
+  debouncedApplyFilter(id);
+};
+
+const showFilters = () => {
+  filtersContainer.classList.remove('img-filters--inactive');
+  filterButtons.forEach((button) => {
+    button.addEventListener('click', onFilterClick);
+  });
+};
+
+export const loadPhotos = () => {
+  getPhotos()
+    .then((photos) => {
+      originalPhotos = photos;
+      displayPhotos(originalPhotos);
+      showFilters();
+    })
+    .catch(() => {
+      showErrorMessage(
+        loadPhotos,
+        'Не удалось загрузить фотографии',
+        'Попробовать снова'
+      );
+    });
 };
